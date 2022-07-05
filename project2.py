39c3d5f7f30d04bcb11173715e2607ca3bde5e9c
leegeunchang <tmsgnsalstj@gmail.com>
2022-07-05 23:37:10 +0900

    change progect2.py

--- project2.py
+++ project2.py
@@ -2,7 +2,8 @@ import pandas as pd
 df = pd.DataFrame({
     'name': ['key', 'lee'],
     'age': [24, 34],
-    'region': ['jeongeup', 'iksan']
+    'region': ['jeongeup', 'iksan'],
+    'sex': ['male', 'female']
 
 })
 print(df)
